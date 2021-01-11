e951b0b6841674618f2b36bae4578f3490e0f9b0
Gille Testard <g.testard@galites.net>
2021-01-11 19:10:39 +0100

    RRR

--- functions/add-reaction.js
+++ functions/add-reaction.js
@@ -1,28 +1,6 @@
-const fetch = require('node-fetch');
-
-exports.handler = async (event) => {
-    // console.log('adding! reaction')
-  const { corgi, type } = JSON.parse(event.body);
-  const response = await fetch('http://138.68.98.19/reactions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      type,
-      corgi,
-    }),
-  })
-    .then(
-        (response) => {
-            console.log('RR', res)
-            response.json()
-        }
-    )
-    .catch((err) => console.error(err));
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-  };
-};
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({message: "Hello World"})
+    };
+}
